4f4e7b99aa4c751fd62e4cf4988c3d2bc3901744
Ayush Chapagain <ac91@rice.edu>
2017-11-22 01:23:29 -0600

    Got Pokémon images to show

--- classification.py
+++ classification.py
@@ -10,22 +10,26 @@ import glob
 
 def load_pokemon_images():
     image_list = []
+    pixel_list = []
 
     for filename in glob.glob('PokemonData/TrainingImages/*.png'):  # assuming gif
         im = Image.open(filename)
-        image_list.append(im)
+        rgb_im = im.convert('RGB')  # convert each image to 'RGB' from default format (greyscale?)
+
+        # for each pixel in the rgb image
+        width, height = rgb_im.size
+        for x in range(0, width):
+            for y in range(0, height):
+                pixel = rgb_im.getpixel((x, y))  # this is a pixel
+                pixel_list.append(pixel)  # this has every pixel for every image
+                
+        # make a list for testing purposes
+        image_list.append(rgb_im)
+
         # now image_list contains every image object
 
-    return image_list
+    return image_list, pixel_list
 
 
-pk1 = load_pokemon_images()[0]
-pk2 = load_pokemon_images()[200]
-pk3 = load_pokemon_images()[300]
-pk1.show()
-pk2.show()
-pk3.show()
 
-print(load_pokemon_images())  # every image is in this list
-print("end")
 
--- test_classification.py
+++ test_classification.py
@@ -1,5 +1,24 @@
-import classification as classification
+import classification
 
 
 def test_classification():
-    print(classification.load_pokemon_images())
+    image_list, pixel_list = classification.load_pokemon_images()
+    print(len(image_list))
+    print(len(pixel_list))
+
+    pk1 = image_list[0]
+    pk2 = image_list[200]
+    pk3 = image_list[400]
+
+    print(pk1.getpixel((40, 40)))
+    print(pk2.getpixel((40, 40)))
+    print(pk3.getpixel((40, 40)))
+    pk1.show()
+    pk2.show()
+    pk3.show()
+
+
+
+
+test_classification()
+
