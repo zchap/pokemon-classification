c2b45d67d37754aaf92e446ee37134c3dd5e86bf
Ayush Chapagain <ac91@rice.edu>
2017-11-23 00:16:36 -0600

    Extracted stats, converted all to floats. We have S & I now. Combine S & I unit by unit to get X.

--- run_classification.py
+++ run_classification.py
@@ -6,18 +6,25 @@ def run_pokemon_classification():
     # This function runs our classification algorithm using the methods in 'classification'
     vector_i = classification.load_pokemon_images('TestImages')
     vector_s = classification.load_pokemon_stats('PokemonData/UnlabeledTestMetadata.csv')
+    num_pokemon = len(vector_i)
+
+    # Note: each inner vector of vector_s only contains 15 elements, since the type of the pokemon has not been
+    # given (we are supposed to find these labels)
 
     # vector_x = vector_i + vector_s
 
     for p in range(0, len(vector_s)):  # indexing each pokemon
         for s in range(0, len(vector_s[p])):  # indexing each stat for each pokemon
             vector_s[p][s] = float(vector_s[p][s])
-        print(vector_s[p])  # here for testing purposes
 
-    return vector_s  # after every inner vector of vector_s has been converted to a float for easy arithmetic
-    # vector_s begins with the data for "Type" because the number has been removed in 'load_pokemon_stats' in
-    # classification.py
+    vector_x = []
+    for i in range(0, num_pokemon):
+        combined_vec = vector_s[i] + vector_i[i]
+        vector_x.append(combined_vec)
+
+    return vector_x
 
 
-run_pokemon_classification()  # expected: 201 vectors of floats, given 201 vectors of floats
+test_classification.print_vector(run_pokemon_classification()[0])
+# expected: 201 vectors of floats, given 201 vectors of floats
 
--- test_classification.py
+++ test_classification.py
@@ -3,12 +3,17 @@ import unittest
 
 # global variables for testing
 images_list = classification.load_pokemon_images('TestImages')[0]
-stats_list = classification.load_pokemon_stats('PokemonData/TrainingMetadata.csv')
+stats_list = classification.load_pokemon_stats('PokemonData/UnlabeledTestMetadata.csv')
 
 
 # Print tests that should pass by basic observation
 def print_stats_list():
-    print(len(stats_list))
+    print(stats_list)
+
+
+def print_vector(input_vector):
+    for vector in input_vector:
+        print(vector)
 
 
 class TestClassification(unittest.TestCase):
@@ -17,13 +22,14 @@ class TestClassification(unittest.TestCase):
         self.assertNotEqual(len(images_list), 96*94)  # should pass
 
     def test_stats_load(self):
-        # expected size of list below should be 601 (first row has been removed)
-        self.assertEqual(len(stats_list), 601)
+        # expected size of list below should be 601 (first row has been removed so it is just the # of pokemon)
+        self.assertEqual(len(stats_list), 201)
 
-        # expected size of each inner list is 16 (pokemon number has been removed, so data starts with "Type")
-        self.assertEqual(len(stats_list[0]), 16)
+        # expected size of each inner list is 15 (pokemon number has been removed, so data starts with "Type")
+        self.assertEqual(len(stats_list[0]), 15)
 
 
+print_stats_list()
 if __name__ == '__main__':
     unittest.main()
 
