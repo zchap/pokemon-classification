6c8ecc75685ed6b13fe14292cafd18afb5c67d7d
Ayush Chapagain <ac91@rice.edu>
2017-11-22 20:42:18 -0600

    Cleaned comments

--- classification.py
+++ classification.py
@@ -8,28 +8,26 @@ from PIL import Image
 import glob
 
 
-def load_pokemon_images():
+def load_pokemon_images(folder):
     image_list = []
-    pixel_list = []
 
-    for filename in glob.glob('PokemonData/TrainingImages/*.png'):  # assuming gif
+    for filename in glob.glob('PokemonData/' + folder + '/*.png'):
         im = Image.open(filename)
         rgb_im = im.convert('RGB')  # convert each image to 'RGB' from default format (greyscale?)
+        pixel_list = []
 
         # for each pixel in the rgb image
         width, height = rgb_im.size
         for x in range(0, width):
             for y in range(0, height):
-                pixel = rgb_im.getpixel((x, y))  # this is a pixel
-                pixel_list.append(pixel)  # this has every pixel for every image
-                
-        # make a list for testing purposes
-        image_list.append(rgb_im)
+                pixel = rgb_im.getpixel((x, y))  # this is a pixel. Each pixel is a tuple object: (r, g, b)
+                pixel_list.append(pixel)  # this has every pixel for current image. Each row in the pixel_list matrix
+                # containing a long x*y row vector.
 
-        # now image_list contains every image object
+        # now image_list contains every image object in the current folder in the form of lists of all pixels for that
+        # image (i.e. [[(r1,g1,b1),(r2,g2,b2),...,(r(x*y),g(x*y),b(x*y)],
+        # [(r1,g1,b1),(r2,g2,b2),...,(r(x*y),g(x*y),b(x*y)], ..., [(r1,g1,b1),(r2,g2,b2),...,(r(x*y),g(x*y),b(x*y)]]
+        # the size of the list above is the number of images (test: 200, training: 601)
+        image_list.append(pixel_list)
 
-    return image_list, pixel_list
-
-
-
-
+    return image_list  
--- test_classification.py
+++ test_classification.py
@@ -2,22 +2,7 @@ import classification
 
 
 def test_classification():
-    image_list, pixel_list = classification.load_pokemon_images()
-    print(len(image_list))
-    print(len(pixel_list))
-
-    pk1 = image_list[0]
-    pk2 = image_list[200]
-    pk3 = image_list[400]
-
-    print(pk1.getpixel((40, 40)))
-    print(pk2.getpixel((40, 40)))
-    print(pk3.getpixel((40, 40)))
-    pk1.show()
-    pk2.show()
-    pk3.show()
-
-
+    print(len(classification.load_pokemon_images()[0]))
 
 
 test_classification()
