a59a2aae3b1d9ed8ddf9a73f75fc2147e6ed0d75
Ronaldo <rbs4@rice.edu>
2017-12-06 16:31:28 -0600

    if you have a c++ compiler run this

--- naldotest.py
+++ naldotest.py
@@ -1,58 +1,57 @@
-import lasagne
 import numpy
-from lasagne import layers
-from lasagne.updates import nesterov_momentum
-from nolearn.lasagne import NeuralNet
-from nolearn.lasagne import visualize
 import classification
+
+from lasagne.layers import DenseLayer
+from lasagne.layers import InputLayer
+from lasagne.layers import DropoutLayer
+from lasagne.layers import Conv2DLayer
+from lasagne.layers import MaxPool2DLayer
+from lasagne.nonlinearities import softmax
+from lasagne.updates import adam
+from nolearn.lasagne import NeuralNet
+from nolearn.lasagne import TrainSplit
 
 xTestImages = numpy.array(classification.load_pokemon_images('TrainingImages'))
 xTestImages = xTestImages.reshape(-1,3,96,96)
+y = classification.load_training_labels('PokemonData/TrainingMetadata.csv')
 
+layersOfCNN = [
+    # layer dealing with the input data
+    (InputLayer, {'shape': (None, xTestImages.shape[1], xTestImages.shape[2], xTestImages.shape[3])}),
 
-# convolutionalNeuralNet = NeuralNet(
-#     layers=[('input', layers.InputLayer),
-#             ('conv2d1', layers.Conv2DLayer),
-#             ('maxpool1', layers.MaxPool2DLayer),
-#             ('conv2d2', layers.Conv2DLayer),
-#             ('maxpool2', layers.MaxPool2DLayer),
-#             ('dropout1', layers.DropoutLayer),
-#             ('dense', layers.DenseLayer),
-#             ('dropout2', layers.DropoutLayer),
-#             ('output', layers.DenseLayer),
-#             ],
-#     # input layer
-#     input_shape=(None, 1, 28, 28),
-#     # layer conv2d1
-#     conv2d1_num_filters=32,
-#     conv2d1_filter_size=(5, 5),
-#     conv2d1_nonlinearity=lasagne.nonlinearities.rectify,
-#     conv2d1_W=lasagne.init.GlorotUniform(),
-#     # layer maxpool1
-#     maxpool1_pool_size=(2, 2),
-#     # layer conv2d2
-#     conv2d2_num_filters=32,
-#     conv2d2_filter_size=(5, 5),
-#     conv2d2_nonlinearity=lasagne.nonlinearities.rectify,
-#     # layer maxpool2
-#     maxpool2_pool_size=(2, 2),
-#     # dropout1
-#     dropout1_p=0.5,
-#     # dense
-#     dense_num_units=256,
-#     dense_nonlinearity=lasagne.nonlinearities.rectify,
-#     # dropout2
-#     dropout2_p=0.5,
-#     # output
-#     output_nonlinearity=lasagne.nonlinearities.softmax,
-#     output_num_units=10,
-#     # optimization method params
-#     update=nesterov_momentum,
-#     update_learning_rate=0.01,
-#     update_momentum=0.9,
-#     max_epochs=10,
-#     verbose=1,
-#     )
-# # Train the network
-# nn = convolutionalNeuralNet.fit(X_train, y_train)
-#
+    # first stage of our convolutional layers
+    (Conv2DLayer, {'num_filters': 96, 'filter_size': 5}),
+    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
+    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
+    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
+    (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
+    (MaxPool2DLayer, {'pool_size': 2}),
+
+    # second stage of our convolutional layers
+    (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
+    (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
+    (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
+    (MaxPool2DLayer, {'pool_size': 2}),
+
+    # two dense layers with dropout
+    (DenseLayer, {'num_units': 64}),
+    (DropoutLayer, {}),
+    (DenseLayer, {'num_units': 64}),
+
+    # the output layer
+    (DenseLayer, {'num_units': 18, 'nonlinearity': softmax}),
+]
+
+convolutionalNeuralNet = NeuralNet(
+    layers=layersOfCNN,
+    max_epochs=10,
+
+    update=adam,
+    update_learning_rate=0.0002,
+
+    objective_l2=0.0025,
+
+    train_split=TrainSplit(eval_size=0.25),
+    verbose=1,
+)
+convolutionalNeuralNet.fit(xTestImages, y)
